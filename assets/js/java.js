de024cba9902d801901d543a81907c929cd1aa3f
Joao Luiz <contato.joaolfc@hotmail.com>
2019-06-06 16:51:21 -0300

    1/2 Responsividade

--- assets/js/java.js
+++ assets/js/java.js
@@ -10,3 +10,14 @@ $('#banner_1').slick({
 
 
 });	
+
+
+
+function toggleMenu(){
+	var menu = document.getElementById("menu");
+	if(menu.style.display == "none"){
+		menu.style.display = "block";
+	}else{
+		menu.style.display = "none";
+	}
+}
